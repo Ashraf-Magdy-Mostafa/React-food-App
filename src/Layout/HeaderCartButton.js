65cc38467078051a2f46547ea49340a5dfa2f491
Ashraf Magdy <ashrafmagdy926@gmail.com>
2023-11-23 08:00:07 +0200

    tweaked Cart animation

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,14 +1,31 @@
 import CartIcon from "../Cart/CartIcon";
 import "./HeaderCartButton.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../Store/Cart-Context";
 const HeaderCartButton = (props) => {
+  const [animateButton, setAnimateButton] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setAnimateButton(true);
+    setTimeout(() => {
+      setAnimateButton(false);
+    }, 400);
+    return () => clearTimeout;
+  }, [items]);
+
   const cartItems = ctx.items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
+
   return (
-    <button className="button" onClick={props.show}>
+    <button
+      className={` button ${animateButton ? "bump" : ""}`}
+      onClick={props.show}
+    >
       <span className="icorrn">
         <CartIcon />
       </span>
